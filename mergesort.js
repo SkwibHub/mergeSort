3cec28b8cfc37ac7a97d44a577b58205c31b3f17
SkwibHub <eidolongroups@gmail.com>
2018-11-05 14:44:23 -0800

    New changes

--- mergesort.js
+++ mergesort.js
@@ -1,1 +1,24 @@
-// Hi Dan
+function split(wholeArray) {
+    let firstHalf = [];
+    let secondHalf = [];
+    fulcrum = Math.ceil(wholeArray.length / 2);
+    firstHalf = wholeArray.slice(0, fulcrum);
+    secondHalf = wholeArray.slice(fulcrum, wholeArray.length);
+    return [firstHalf, secondHalf];
+}
+
+function merge(firstHalf, secondHalf) {
+
+    /* your code here to define the firstHalf and secondHalf arrays */
+
+    return wholeArray;
+}
+
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+
+
+} // Hi Dan
