a5a5f0a774e9e276535be71c3e719d9bc98a2216
za_git <26679283+zachhubbard77@users.noreply.github.com>
2018-11-05 15:30:31 -0800

    merge function

--- mergesort.js
+++ mergesort.js
@@ -1,23 +1,50 @@
+
 function split(wholeArray) {
+    if (wholeArray.length <= 1) {
+        return wholeArray;
+    }
     let firstHalf = [];
     let secondHalf = [];
-    fulcrum = Math.ceil(wholeArray.length / 2);
+    let fulcrum = Math.floor(wholeArray.length / 2);
     firstHalf = wholeArray.slice(0, fulcrum);
     secondHalf = wholeArray.slice(fulcrum, wholeArray.length);
-    return [firstHalf, secondHalf];
+
+
+
+    return [firstHalf,secondHalf];
 }
 
 function merge(firstHalf, secondHalf) {
 
     /* your code here to define the firstHalf and secondHalf arrays */
+    //firstHalf[0]  secondHalf[0]
+    // finalArr.push(lesser of firstHalf[0] or second........)
+    // push remaining if one array.length is 0;
+    let finalArr = [];
+    while(firstHalf.length > 0 && secondHalf.length > 0){
+        if(firstHalf[0] < secondHalf[0]){
+            finalArr.push(firstHalf[0]);
+            firstHalf.shift();
+        } else if (firstHalf[0] >= secondHalf[0]) {
+            finalArr.push(secondHalf[0]);
+            secondHalf.shift();
+        }
+    }
 
-    return wholeArray;
+    if(firstHalf.length === 0){
+        return [...finalArr, ...secondHalf]     
+    } 
+
+    if(secondHalf.length === 0){
+        return [...finalArr, ...firstHalf]  
+    }
+
 }
 
+
+
 function mergeSort(array) {
-    if (array.length <= 1) {
-        return array;
-    }
+   
 
 
 
